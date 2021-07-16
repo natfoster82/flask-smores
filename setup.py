46ce1f497874aa84fa2a9a935e41a9f904a720b3
nat <natfoster@gmail.com>
2021-07-16 10:17:10 -0600

    0.2.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Smores',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/natfoster82/flask-smores',
     license='MIT',
     author='Nat Foster',
